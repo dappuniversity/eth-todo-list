8ee52ef34df1cdc93514cb10b11067e16abb8293
Sanjay S <sanjusuresh2001@gmail.com>
2022-08-26 11:35:23 +0530

    Referenced .min file in index.html

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ App = {
     await App.loadAccount()
     await App.loadContract()
     await App.render()
+    web3.eth.defaultAccount = web3.eth.accounts[0]
   },
 
   // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
